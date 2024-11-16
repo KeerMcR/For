d044964c16bbe1455c0c5b012f0f322e086133b6
KeerMcR <119884975+KeerMcR@users.noreply.github.com>
2024-11-16 19:52:05 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -5,7 +5,7 @@ not_primes = []
 for number in numbers:
     if number > 1:  # Исключаем единицу из перебора
         is_prime = True
-        for i in range(2, int(number / 2) + 1):  # Проверяем делители до половины числа, поскольку дальше нет смысла
+        for i in range(2, number // 2 + 1):  # Проверяем делители до половины числа, поскольку дальше нет смысла
             if number % i == 0:
                 is_prime = False
         if is_prime:
